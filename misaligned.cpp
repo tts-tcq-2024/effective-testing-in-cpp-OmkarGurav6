3fb28335fd4912e8a07abd13a7af77586257e194
Omkar Gurav <64888669+OmkarGurav6@users.noreply.github.com>
2024-08-14 13:43:43 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -51,8 +51,8 @@ void testColorPairs()
 
 int main() {
     int result = printColorMap();
-    assert(result == 25);
     testColorPairs();
+    assert(result == 25);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
