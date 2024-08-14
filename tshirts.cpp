b23e056e91c54974c3b925ac60cf6c13bb927017
Omkar Gurav <64888669+OmkarGurav6@users.noreply.github.com>
2024-08-14 13:14:06 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -16,7 +16,7 @@ char size(int cms) {
 int main() {
     assert(size(38) == 'S');
     assert(size(40) == 'M');
-    assert(size(43) == 'L');
+    assert(size(42) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
